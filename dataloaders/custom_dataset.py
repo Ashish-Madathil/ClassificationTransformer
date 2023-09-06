e56482eefb33038a9f6869f0ba492fbfefbf932c
Ashish <amadathil@progenesis.com>
2023-09-06 11:25:05 +0530

    added custom dataset

---
+++ dataloaders/custom_dataset.py
@@ -0,0 +1,11 @@
+#multiclass image classification model and a training script for a custom dataset that pulls from a csv file consisting of image paths in the first column and an integer of any value between 0 and 4 in the second column. Use PyTorch.
+
+import torch
+from torch.utils.data import Dataset, Dataloader
+from torchvision import transforms
+import pandas as pd
+from PIL import Image
+
+class CustomDataset(Dataset):
+    def __init__ (self, csv_path, transform=None):
+        se
